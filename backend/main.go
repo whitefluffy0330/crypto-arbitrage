b9b3a5e652ca006788aa1dafc0d461ddfd4fd5c5
whitefluffy0330 <suicidehorizont@gmail.com>
2025-04-29 14:23:30 +0300

    Update main.go

--- backend/main.go
+++ backend/main.go
@@ -167,23 +167,30 @@ func main() {
 		bot.Send(msg)
 	}
 }
-    if goalCreationInProgress {
-        if tempGoalName == "" {
-            tempGoalName = message.Text
-            msg := tgbotapi.NewMessage(message.Chat.ID, "Введіть бажану суму доходу:")
-            bot.Send(msg)
-        } else if tempGoalAmount == "" {
-            tempGoalAmount = message.Text
-            values := []interface{}{tempGoalName, tempGoalAmount}
-            writeRow("Мапа доходу", values)
-            tempGoalName = ""
-            tempGoalAmount = ""
-            goalCreationInProgress = false
-            msg := tgbotapi.NewMessage(message.Chat.ID, "Ціль додано!")
+            case "Вихідний день":
+            writeRow("Робочі сесії", []interface{}{time.Now().Format("02.01.2006 15:04")})
+            isWorking = false
+            msg := tgbotapi.NewMessage(message.Chat.ID, "Вихідний день зафіксовано!")
             bot.Send(msg)
         }
-        return
-    }
+
+        if goalCreationInProgress {
+            if tempGoalName == "" {
+                tempGoalName = message.Text
+                msg := tgbotapi.NewMessage(message.Chat.ID, "Введіть бажану суму:")
+                bot.Send(msg)
+            } else if tempGoalAmount == "" {
+                tempGoalAmount = message.Text
+                values := []interface{}{tempGoalName, tempGoalAmount}
+                writeRow("Мапа доходу", values)
+                tempGoalName = ""
+                tempGoalAmount = ""
+                goalCreationInProgress = false
+                msg := tgbotapi.NewMessage(message.Chat.ID, "Ціль додано!")
+                bot.Send(msg)
+            }
+            return
+        }
 func sendStartKeyboard(chatID int64) {
 	msg := tgbotapi.NewMessage(chatID, "Оберіть дію:")
 	msg.ReplyMarkup = tgbotapi.NewReplyKeyboard(
