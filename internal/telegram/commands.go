fa2ea5f4787bc18a5a24ca6d164592d006f2684f
whitefluffy0330 <suicidehorizont@gmail.com>
2025-05-03 15:51:25 +0300

    Update commands.go

--- internal/telegram/commands.go
+++ internal/telegram/commands.go
@@ -2,9 +2,26 @@ package telegram
 
 import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 
-func StartWork(bot *tgbotapi.BotAPI, msg *tgbotapi.Message)                                    {}
-func StopWork(bot *tgbotapi.BotAPI, msg *tgbotapi.Message)                                     {}
-func DayOff(bot *tgbotapi.BotAPI, msg *tgbotapi.Message, srv any, spreadsheetID string)        {}
-func ShowMainKeyboard(bot *tgbotapi.BotAPI, cfg any, chatID int64)                             {}
-func HandleButtons(bot *tgbotapi.BotAPI, msg *tgbotapi.Message, srv any, spreadsheetID string) {}
-func HandleCallback(bot *tgbotapi.BotAPI, cb *tgbotapi.CallbackQuery)                          {}
+func StartWork(bot *tgbotapi.BotAPI, msg *tgbotapi.Message) {
+	// TODO: Реалізувати логіку старту роботи
+}
+
+func StopWork(bot *tgbotapi.BotAPI, msg *tgbotapi.Message) {
+	// TODO: Реалізувати логіку зупинки роботи
+}
+
+func DayOff(bot *tgbotapi.BotAPI, msg *tgbotapi.Message, srv any, spreadsheetID string) {
+	// TODO: Реалізувати логіку дня відпочинку
+}
+
+func ShowMainKeyboard(bot *tgbotapi.BotAPI, cfg any, chatID int64) {
+	// TODO: Відправити головну клавіатуру
+}
+
+func HandleButtons(bot *tgbotapi.BotAPI, msg *tgbotapi.Message, srv any, spreadsheetID string) {
+	// TODO: Обробка кнопок
+}
+
+func HandleCallback(bot *tgbotapi.BotAPI, cb *tgbotapi.CallbackQuery) {
+	// TODO: Обробка callback-даних
+}
