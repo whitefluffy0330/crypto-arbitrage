d1254277127a8d94d0193951dd6480edcd9f355e
whitefluffy0330-helper <suicidehorizont@hotmail.com>
2025-05-05 21:35:49 +0300

    Update closegoal.go

--- internal/telegram/closegoal.go
+++ internal/telegram/closegoal.go
@@ -2,33 +2,18 @@ package telegram
 
 import (
 	"fmt"
-	"log"
-	"strings"
-	"time"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-	"google.golang.org/api/sheets/v4"
 )
 
-// HandleCloseGoalInput обробляє завершення цілі
-func HandleCloseGoalInput(bot *tgbotapi.BotAPI, message *tgbotapi.Message, srv *sheets.Service, spreadsheetID string) {
-	userID := fmt.Sprintf("%d", message.Chat.ID)
-	finalText := strings.TrimSpace(message.Text)
-	now := time.Now().Format("02.01.2006")
+// HandleCloseGoalInput обробляє повідомлення про завершення цілі
+func HandleCloseGoalInput(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
+	chatID := message.Chat.ID
+	userInput := message.Text
 
-	writeRange := userID + "!A3:B3"
-	valueRange := &sheets.ValueRange{
-		Values: [][]interface{}{{now, finalText}},
-	}
+	// Тут можна реалізувати логіку для збереження завершеної цілі, якщо потрібно
+	response := fmt.Sprintf("✅ Ціль '%s' успішно закрита!", userInput)
 
-	_, err := srv.Spreadsheets.Values.Update(spreadsheetID, writeRange, valueRange).ValueInputOption("RAW").Do()
-	if err != nil {
-		log.Printf("❌ Не вдалося завершити ціль: %v", err)
-		msg := tgbotapi.NewMessage(message.Chat.ID, "Помилка при завершенні цілі.")
-		bot.Send(msg)
-		return
-	}
-
-	msg := tgbotapi.NewMessage(message.Chat.ID, "🎉 Ціль успішно завершено! Ти молодець!")
+	msg := tgbotapi.NewMessage(chatID, response)
 	bot.Send(msg)
 }
