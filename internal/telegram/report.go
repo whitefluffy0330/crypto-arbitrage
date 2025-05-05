86d396aa9567f635819b30a98531e691ce54a0dd
whitefluffy0330-helper <suicidehorizont@hotmail.com>
2025-05-05 21:02:51 +0300

    Update report.go

--- internal/telegram/report.go
+++ internal/telegram/report.go
@@ -1,22 +1,25 @@
 package telegram
 
 import (
+	"fmt"
 	"log"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/whitefluffy0330/crypto-arbitrage/internal/sheets"
+	"google.golang.org/api/sheets/v4"
 )
 
-// HandleProgressReportCommand обробляє команду /progress
-func HandleProgressReportCommand(bot *tgbotapi.BotAPI, message *tgbotapi.Message, srv *sheets.Service, spreadsheetID string) {
-	report, err := sheets.GenerateProgressReport(srv, spreadsheetID, message.Chat.ID)
+// HandleProgressReport надсилає звіт користувачу
+func HandleProgressReport(bot *tgbotapi.BotAPI, message *tgbotapi.Message, srv *sheets.Service, spreadsheetID string) {
+	userID := fmt.Sprintf("%d", message.Chat.ID)
+	reportText, err := sheets.GenerateProgressReport(srv, spreadsheetID, userID)
 	if err != nil {
-		log.Printf("❌ Помилка генерації звіту: %v", err)
-		msg := tgbotapi.NewMessage(message.Chat.ID, "Не вдалося згенерувати звіт 😢")
+		log.Printf("❌ Помилка при генерації звіту: %v", err)
+		msg := tgbotapi.NewMessage(message.Chat.ID, "Не вдалося згенерувати звіт.")
 		bot.Send(msg)
 		return
 	}
 
-	msg := tgbotapi.NewMessage(message.Chat.ID, report)
+	msg := tgbotapi.NewMessage(message.Chat.ID, reportText)
 	bot.Send(msg)
 }
