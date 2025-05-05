edaf7ba3e3a92613401665641b964e16464bfdc5
whitefluffy0330-helper <suicidehorizont@hotmail.com>
2025-05-05 22:01:16 +0300

    Update handler.go

--- internal/telegram/handler.go
+++ internal/telegram/handler.go
@@ -1,18 +1,38 @@
 package telegram
 
 import (
-	"fmt"
+	"log"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"google.golang.org/api/sheets/v4"
 
-	internalSheets "github.com/whitefluffy0330/crypto-arbitrage/internal/sheets"
+	"github.com/whitefluffy0330/crypto-arbitrage/internal/telegram/commands"
+	"github.com/whitefluffy0330/crypto-arbitrage/internal/telegram/goal"
+	"github.com/whitefluffy0330/crypto-arbitrage/internal/telegram/keyboard"
 )
 
-// ReportProgress генерує та надсилає звіт користувачу
-func ReportProgress(bot *tgbotapi.BotAPI, message *tgbotapi.Message, srv *sheets.Service, spreadsheetID string) {
-	chatID := message.Chat.ID
+// HandleUpdate обробляє повідомлення та callback-и
+func HandleUpdate(bot *tgbotapi.BotAPI, update tgbotapi.Update, srv *sheets.Service, spreadsheetID string) {
+	if update.Message != nil {
+		switch update.Message.Text {
+		case "/start":
+			keyboard.ShowMainKeyboard(bot, update.Message.Chat.ID)
+		case "Почати роботу":
+			commands.StartWork(bot, update.Message)
+		case "Завершити роботу":
+			commands.StopWork(bot, update.Message)
+		case "Взяти вихідний":
+			commands.DayOff(bot, update.Message, srv, spreadsheetID)
+		case "Моя ціль":
+			goal.HandleMyGoalCommand(bot, update.Message.Chat.ID)
+		case "Звіт":
+			ReportProgress(bot, update.Message, srv, spreadsheetID)
+		default:
+			keyboard.ShowMainKeyboard(bot, update.Message.Chat.ID)
+		}
+	}
 
-	report := internalSheets.GenerateProgressReport(srv, spreadsheetID)
-	bot.Send(tgbotapi.NewMessage(chatID, report))
+	if update.CallbackQuery != nil {
+		goal.HandleCallback(bot, update.CallbackQuery)
+	}
 }
