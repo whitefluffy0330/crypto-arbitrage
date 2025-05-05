537e2d20e51991dc0deec6090fdcc857a9ed5789
whitefluffy0330-helper <suicidehorizont@hotmail.com>
2025-05-05 20:52:24 +0300

    Update goal.go

--- internal/telegram/goal.go
+++ internal/telegram/goal.go
@@ -1,51 +1,38 @@
 package telegram
 
 import (
-	"fmt"
 	"log"
-	"time"
+	"strconv"
+	"strings"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"google.golang.org/api/sheets/v4"
 )
 
-var (
-	goalState      string
-	tempGoalName   string
-	tempGoalAmount string
-)
+// HandleGoalInput обробляє введення цілі
+func HandleGoalInput(bot *tgbotapi.BotAPI, message *tgbotapi.Message, srv *sheets.Service, spreadsheetID string) {
+	userID := strconv.FormatInt(message.Chat.ID, 10)
+	goalText := strings.TrimSpace(message.Text)
 
-func HandleGoalCommand(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
-	goalState = "waiting_goal_name"
-	msg := tgbotapi.NewMessage(message.Chat.ID, "Введи назву своєї нової цілі:")
+	writeRange := userID + "!A2"
+	valueRange := &sheets.ValueRange{
+		Values: [][]interface{}{{goalText}},
+	}
+
+	_, err := srv.Spreadsheets.Values.Update(spreadsheetID, writeRange, valueRange).ValueInputOption("RAW").Do()
+	if err != nil {
+		log.Printf("❌ Не вдалося зберегти ціль: %v", err)
+		msg := tgbotapi.NewMessage(message.Chat.ID, "Помилка при збереженні цілі.")
+		bot.Send(msg)
+		return
+	}
+
+	msg := tgbotapi.NewMessage(message.Chat.ID, "✅ Ціль збережена! Тепер працюй над нею кожного дня!")
 	bot.Send(msg)
 }
 
-func HandleGoalInput(bot *tgbotapi.BotAPI, message *tgbotapi.Message, srv *sheets.Service, spreadsheetID string) {
-	switch goalState {
-	case "waiting_goal_name":
-		tempGoalName = message.Text
-		goalState = "waiting_goal_amount"
-		msg := tgbotapi.NewMessage(message.Chat.ID, "Яка сума ($) потрібна для цієї цілі?")
-		bot.Send(msg)
-
-	case "waiting_goal_amount":
-		tempGoalAmount = message.Text
-		err := writeGoalRow(srv, spreadsheetID, []interface{}{tempGoalName, tempGoalAmount, 0, "Активна", time.Now().Format("02.01.2006"), "0%"})
-		if err != nil {
-			log.Printf("Помилка запису цілі: %v", err)
-		}
-		goalState = ""
-		tempGoalName = ""
-		tempGoalAmount = ""
-		msg := tgbotapi.NewMessage(message.Chat.ID, "🎯 Ціль додано! Тепер працюємо над її досягненням!")
-		bot.Send(msg)
-	}
-}
-
-func writeGoalRow(srv *sheets.Service, spreadsheetID string, values []interface{}) error {
-	_, err := srv.Spreadsheets.Values.Append(spreadsheetID, "Цілі!A:F", &sheets.ValueRange{
-		Values: [][]interface{}{values},
-	}).ValueInputOption("USER_ENTERED").Do()
-	return err
+// HandleCallback обробляє callback-запити (наприклад, від inline-кнопок)
+func HandleCallback(bot *tgbotapi.BotAPI, callback *tgbotapi.CallbackQuery) {
+	msg := tgbotapi.NewMessage(callback.Message.Chat.ID, "Обробка callback: "+callback.Data)
+	bot.Send(msg)
 }
