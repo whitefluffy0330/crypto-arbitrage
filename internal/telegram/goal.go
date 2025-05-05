a8d7a6429a3b2f7024f4e92f26e10cc0b3f45167
whitefluffy0330-helper <suicidehorizont@hotmail.com>
2025-05-05 21:52:43 +0300

    Update goal.go

--- internal/telegram/goal.go
+++ internal/telegram/goal.go
@@ -5,12 +5,21 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-// HandleGoalInput обробляє введену користувачем ціль
+// userGoals зберігає активні цілі користувачів
+var userGoals = make(map[int64]string)
+
+// HandleGoalInput обробляє введення користувачем цілі
 func HandleGoalInput(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
 	chatID := message.Chat.ID
-	goal := message.Text
+	userGoals[chatID] = message.Text
 
-	response := fmt.Sprintf("🎯 Твоя нова ціль: \"%s\" записана!", goal)
-	msg := tgbotapi.NewMessage(chatID, response)
+	msg := tgbotapi.NewMessage(chatID, fmt.Sprintf("🎯 Ціль збережено: %s", message.Text))
+	bot.Send(msg)
+}
+
+// HandleCallback — шаблон обробки callback-кнопок
+func HandleCallback(bot *tgbotapi.BotAPI, callback *tgbotapi.CallbackQuery) {
+	// Тут можна реалізувати логіку натискання кнопок
+	msg := tgbotapi.NewMessage(callback.Message.Chat.ID, "🔘 Натиснута кнопка: "+callback.Data)
 	bot.Send(msg)
 }
