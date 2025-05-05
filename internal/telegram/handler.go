609450458119c9673862af131f101431d2d9f1ad
whitefluffy0330-helper <suicidehorizont@hotmail.com>
2025-05-05 20:34:22 +0300

    Update handler.go

--- internal/telegram/handler.go
+++ internal/telegram/handler.go
@@ -1,36 +1,37 @@
 package telegram
 
 import (
-	"fmt"
+	"log"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-	"google.golang.org/api/sheets/v4"
 
+	"github.com/whitefluffy0330/crypto-arbitrage/internal/sheets"
+	"github.com/whitefluffy0330/crypto-arbitrage/internal/telegram/commands"
 	"github.com/whitefluffy0330/crypto-arbitrage/internal/telegram/goal"
 	"github.com/whitefluffy0330/crypto-arbitrage/internal/telegram/keyboard"
-	"github.com/whitefluffy0330/crypto-arbitrage/internal/telegram/motivation"
-	"github.com/whitefluffy0330/crypto-arbitrage/internal/telegram/commands"
 )
 
-func HandleUpdate(bot *tgbotapi.BotAPI, update tgbotapi.Update, srv *sheets.Service, spreadsheetID string) {
-	if update.Message != nil {
-		switch update.Message.Text {
-		case "Почати роботу":
-			commands.StartWork(bot, update.Message)
-		case "Завершити роботу":
-			commands.StopWork(bot, update.Message)
-		case "Взяти вихідний":
-			commands.DayOff(bot, update.Message, srv, spreadsheetID)
-		case "Моя ціль":
-			goal.HandleMyGoalCommand(bot, update.Message.Chat.ID, srv, spreadsheetID)
-		case "Мотивація":
-			motivation.SendRandomMotivation(bot, update.Message.Chat.ID)
-		default:
-			keyboard.ShowMainKeyboard(bot, update.Message.Chat.ID)
+func HandleUpdates(bot *tgbotapi.BotAPI, updates tgbotapi.UpdatesChannel, srv *sheets.Service, spreadsheetID string) {
+	for update := range updates {
+		if update.Message != nil {
+			switch update.Message.Text {
+			case "🔁 Старт":
+				commands.StartWork(bot, update.Message)
+			case "⛔️ Стоп":
+				commands.StopWork(bot, update.Message)
+			case "🏖 Вихідний":
+				commands.DayOff(bot, update.Message, srv, spreadsheetID)
+			case "🎯 Моя ціль":
+				goal.HandleMyGoalCommand(bot, update.Message.Chat.ID, srv, spreadsheetID)
+			case "📊 Прогрес":
+				ReportProgress(bot, update.Message.Chat.ID, srv, spreadsheetID)
+			default:
+				keyboard.ShowMainKeyboard(bot, update.Message.Chat.ID)
+			}
 		}
-	}
 
-	if update.CallbackQuery != nil {
-		goal.HandleCallback(bot, update.CallbackQuery)
+		if update.CallbackQuery != nil {
+			goal.HandleCallback(bot, update.CallbackQuery)
+		}
 	}
 }
