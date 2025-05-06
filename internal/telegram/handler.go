b9e58fe91291f4b08d9808acc04395ada627aa43
whitefluffy0330-helper <suicidehorizont@hotmail.com>
2025-05-06 18:00:49 +0300

    Update handler.go

--- internal/telegram/handler.go
+++ internal/telegram/handler.go
@@ -4,19 +4,39 @@ import (
 	"log"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-	sheetsAPI "google.golang.org/api/sheets/v4"
+	"google.golang.org/api/sheets/v4"
 
-	"github.com/whitefluffy0330/crypto-arbitrage/internal/sheets"
+	"github.com/whitefluffy0330/crypto-arbitrage/internal/telegram/commands"
 	"github.com/whitefluffy0330/crypto-arbitrage/internal/telegram/goal"
 	"github.com/whitefluffy0330/crypto-arbitrage/internal/telegram/keyboard"
+	"github.com/whitefluffy0330/crypto-arbitrage/internal/telegram/motivation"
+	"github.com/whitefluffy0330/crypto-arbitrage/internal/sheets"
 )
 
-// ReportProgress генерує звіт про дохід і надсилає його користувачу
-func ReportProgress(bot *tgbotapi.BotAPI, msg *tgbotapi.Message, srv *sheetsAPI.Service, spreadsheetID string) {
-	reportText := sheets.GenerateProgressReport(srv, spreadsheetID)
+// HandleUpdate обробляє вхідні повідомлення та callback-и
+func HandleUpdate(bot *tgbotapi.BotAPI, update tgbotapi.Update, srv *sheets.Service, spreadsheetID string) {
+	if update.Message != nil {
+		switch update.Message.Text {
+		case "/start":
+			commands.StartWork(bot, update.Message)
+		case "/stop":
+			commands.StopWork(bot, update.Message)
+		case "/dayoff":
+			commands.DayOff(bot, update.Message, srv, spreadsheetID)
+		case "/goal":
+			goal.HandleMyGoalCommand(bot, update.Message.Chat.ID)
+		case "/motivation":
+			motivationText := motivation.GetRandomMotivation()
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, motivationText)
+			bot.Send(msg)
+		case "/report":
+			ReportProgress(bot, update.Message, srv, spreadsheetID)
+		default:
+			keyboard.ShowMainKeyboard(bot, update.Message.Chat.ID)
+		}
+	}
 
-	response := tgbotapi.NewMessage(msg.Chat.ID, reportText)
-	if _, err := bot.Send(response); err != nil {
-		log.Printf("Помилка надсилання звіту: %v", err)
+	if update.CallbackQuery != nil {
+		goal.HandleCallback(bot, update.CallbackQuery)
 	}
 }
