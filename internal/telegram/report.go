9565af284ce604f43c0e02bff9be9f99efe6b2c3
whitefluffy0330-helper <suicidehorizont@hotmail.com>
2025-05-06 15:28:57 +0300

    Update report.go

--- internal/telegram/report.go
+++ internal/telegram/report.go
@@ -5,15 +5,16 @@ import (
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	sheetsAPI "google.golang.org/api/sheets/v4"
+
 	"github.com/whitefluffy0330/crypto-arbitrage/internal/sheets"
 )
 
-// Надсилає користувачу звіт про прогрес з Google Sheets
-func SendProgressReport(bot *tgbotapi.BotAPI, msg *tgbotapi.Message, srv *sheetsAPI.Service, spreadsheetID string) {
-	report := sheets.GenerateProgressReport(srv, spreadsheetID)
+// ReportProgress генерує звіт про дохід і надсилає його користувачу
+func ReportProgress(bot *tgbotapi.BotAPI, msg *tgbotapi.Message, srv *sheetsAPI.Service, spreadsheetID string) {
+	reportText := sheets.GenerateProgressReport(srv, spreadsheetID)
 
-	message := tgbotapi.NewMessage(msg.Chat.ID, report)
-	if _, err := bot.Send(message); err != nil {
+	response := tgbotapi.NewMessage(msg.Chat.ID, reportText)
+	if _, err := bot.Send(response); err != nil {
 		log.Printf("Помилка надсилання звіту: %v", err)
 	}
 }
